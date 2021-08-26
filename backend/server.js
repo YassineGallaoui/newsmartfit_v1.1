ad9d9d0d20e035b4614a9a9e3de05715fc95009c
Gallaoui M. Yassine <22yassine05@gmail.com>
2021-08-26 13:36:43 +0200

    added development proxy

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 require('dotenv').config();
 
 
-// QUA SETTIAMO IL NOSTRO SERVER CON EXPRESS
+// SETTIAMO SERVER CON EXPRESS
 const app = express();
-const port = process.env.PORT || 5000;  //PORTA SU CUI ABBIAMO IL SERVER, SE NON NE ABBIAMO UNA DI DEFAULT PRENDE LA 5000
-/* const buildPath = path.join('../build');
- */
+const port = process.env.PORT || 5000;
 
-// QUA SETTIAMO IL NOSTRO MIDDLEWARE
+// SETTIAMO MIDDLEWARE
 app.use(cors());
 app.use(express.json());
-/* app.use(express.urlencoded({ extended: true })); */
 
-//MONGODB E MONGOOSE
-const uri = process.env.ATLAS_URI;  //LINK PER IL NOSTRO DATABASE MONGODB ATLAS
+// MONGODB E MONGOOSE
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'connection error:'))
@@ -28,18 +24,11 @@ connection.once('open', () => {
     console.log("Connessione al database MongoDB avvenuta con successo!")
 })
 
-//HTTP REQUEST LOGGER
-const athleteRouter = require('./routes/athletes'); //DEFINISCO QUALE È LA SORGENTE DEL ROUTER DELLA SEZIONE ATLETI
-const rulesRouter = require('./routes/rules'); //DEFINISCO QUALE È LA SORGENTE DEL ROUTER DELLA SEZIONE RULES
+// HTTP REQUEST LOGGER
+const athleteRouter = require('./routes/athletes');
+const rulesRouter = require('./routes/rules');
 app.use('/athletes', athleteRouter);
 app.use('/rules', rulesRouter);
-
-/* if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../build')));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, '../build', 'index.html'))
-    });
-} */
 
 // FOR PRODUCTION:
 // Step 1:
@@ -49,22 +38,15 @@ app.use('/rules', rulesRouter);
   response.sendFile(path.resolve(__dirname, "./build", "index.html"));
 }); */
 
-//FOR LOCAL DEVELOPMENT: 
-module.exports = function(app) {
-    app.use(
-        createProxyMiddleware(["/*"], { target: "http://localhost:5000" })
-    );
-};
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
+    app.use(express.static("./build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+        res.sendFile(path.resolve(__dirname,  "./build", "index.html"));
     });
 }
 
 
-// METTIAMO IL SERVER IN ASCOLTO SULLA PORTA CHE ABBIAMO SCELTO
+// METTIAMO SERVER IN ASCOLTO SULLA PORTA
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
 })
