3f9af04ddf304a1fe5df345a87e38d4e5d231638
Gallaoui M. Yassine <22yassine05@gmail.com>
2021-08-26 13:47:02 +0200

    added routing on server

--- backend/server.js
+++ backend/server.js
@@ -39,9 +39,12 @@ app.use('/rules', rulesRouter);
 }); */
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("./build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "./build", "index.html"));
+    app.use(express.static(path.join(__dirname, 'build')));
+    app.get('/athletes', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+    app.get('/rules', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
 }
 
