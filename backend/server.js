bee63453861791bd1a34f51826c1b90f21eb9bbf
Gallaoui M. Yassine <22yassine05@gmail.com>
2021-08-26 13:54:20 +0200

    added routing on server

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,9 @@ if (process.env.NODE_ENV === "production") {
     app.get('/rules', function (req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     });
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
 }
 
 
