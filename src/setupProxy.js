1370be62ded0d2d40512af0e5557d1e1077e4e6c
Yassine <22yassine05@gmail.com>
2021-08-27 10:35:55 +0200

    setted correct production proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 let s = "";
 
 if(process.env.NODE_ENV !== "production") {
-  s = 'http://127.0.0.1:' + (process.env.PORT || 5000)
+  s = 'http://newsmartfitv1.herokuapp.com:' + (process.env.PORT || 5000)
   module.exports = function(app) {
     app.use(
       '/athletes',
