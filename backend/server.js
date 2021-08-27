ba054ef925b6c9c441dd6d506f09c7481fe49c6e
Yassine <22yassine05@gmail.com>
2021-08-27 11:30:29 +0200

    other try

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const path = require('path');
-
-
 require('dotenv').config();
+
 
 
 // SETTIAMO SERVER CON EXPRESS
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
 // SETTIAMO MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+
+
 
 // MONGODB E MONGOOSE
 const uri = process.env.ATLAS_URI;
@@ -24,32 +27,25 @@ connection.once('open', () => {
     console.log("Connessione al database MongoDB avvenuta con successo!")
 })
 
+
+
 // HTTP REQUEST LOGGER
 const athleteRouter = require('./routes/athletes');
 const rulesRouter = require('./routes/rules');
 app.use('/athletes', athleteRouter);
 app.use('/rules', rulesRouter);
 
+
+
 // FOR PRODUCTION:
 // Step 1:
-/* app.use(express.static(path.resolve(__dirname, "./build"))); */
+app.use(express.static(path.resolve(__dirname, "./build")));
 // Step 2:
-/* app.get("*", function (request, response) {
+app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./build", "index.html"));
-}); */
+});
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, 'build')));
-    app.get('/athletes', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-    app.get('/rules', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
-}
+
 
 // METTIAMO SERVER IN ASCOLTO SULLA PORTA
 app.listen(port, () => {
