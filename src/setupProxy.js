6a34eb5fb37d47337c53eeb58a3aefac81e86dfb
Yassine <22yassine05@gmail.com>
2021-08-27 10:57:51 +0200

    changed proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-console.log(process.env.NODE_ENV)
+console.log("ciao, il node inv è: " + process.env.NODE_ENV)
 if(process.env.NODE_ENV !== "production") {
   module.exports = function(app) {
     app.use(
